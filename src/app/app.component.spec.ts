515aa474abd9c06e7e159db877d45fdde884cd25
michaelstievenart <michael.l.stievenart@gmail.com>
2018-08-30 08:45:27 +0200

    include multi select in app module

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,11 +1,15 @@
 import { TestBed, async } from '@angular/core/testing';
 import { AppComponent } from './app.component';
+import { MultiRadioSelectModule } from './multi-radio-select/multi-radio-select.module';
 describe('AppComponent', () => {
   beforeEach(async(() => {
     TestBed.configureTestingModule({
       declarations: [
         AppComponent
       ],
+      imports: [
+        MultiRadioSelectModule
+      ]
     }).compileComponents();
   }));
 
